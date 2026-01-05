fd011a0b9ec3a0ac04336e83fb3ecefcd54b7338
trexxet <atomnik07@gmail.com>
2026-01-05 23:12:52 +0500

    Add newline flag to test printer

--- test/basedline_test.cpp
+++ test/basedline_test.cpp
@@ -6,11 +6,12 @@
 #include <stop_token>
 #include <thread>
 
-void test_printer (std::stop_token stop, Basedline::Basedline& bl, int timing, int n) {
+void test_printer (std::stop_token stop, Basedline::Basedline& bl, int timing, bool newline, int n) {
 	std::binary_semaphore sem {0};
 	std::stop_callback cb (stop, [&sem] { sem.release(); });
+	static int i = 0;
 	while (!stop.stop_requested()) {
-		bl.print (std::move (std::format("test {}\n", n)));
+		bl.print (std::move (std::format ("test {} {}{}", n, i++, newline ? "\n" : "")));
 		sem.try_acquire_for (std::chrono::milliseconds (timing));
 	}
 }
@@ -19,13 +20,11 @@ int main() {
 	Basedline::Basedline bl;
 	Basedline::OptString input;
 
-	std::jthread printingThread1 (test_printer, std::ref(bl), 300, 1);
-	std::jthread printingThread2 (test_printer, std::ref(bl), 500, 2);
-	std::jthread printingThread3 (test_printer, std::ref(bl), 700, 3);
+	std::jthread printingThread1 (test_printer, std::ref(bl), 300, false, 1);
+	std::jthread printingThread2 (test_printer, std::ref(bl), 500, true, 2);
+	std::jthread printingThread3 (test_printer, std::ref(bl), 700, true, 3);
 
-	bl.read ("> ");
 	bl.print ("test main\n");
-	while (!(input = bl.loop()));
 	bl.read ("> ");
 	while (!(input = bl.loop()));
 	return 0;
